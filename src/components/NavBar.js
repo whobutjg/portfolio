a0d453451011a9f0a55157c5407956ef0b8f537c
jgiardina <jgiardinajg@gmail.com>
2021-03-16 19:29:02 -0400

    Adds links to menu options

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,19 @@
-import 'materialize-css';
-import jgLogo from '../images/jglogo.jpeg'
+import "materialize-css";
+import jgLogo from "../images/jglogo.jpeg";
 
 const NavBar = () => {
-
   return (
     <div className="navbar">
-      <img src={jgLogo} alt='jglogo' className='jg' />
-      <h3><a href="about">About</a></h3>
-      <h3><a href="work">Work</a></h3>
+      <img src={jgLogo} alt="jglogo" className="jg" />
+      <h3>
+        <a href="https://whobutjg.github.io/portfolio/">About</a>
+      </h3>
+      <h3>
+        <a href="https://github.com/whobutjg">Work</a>
+      </h3>
       <h3>Contact</h3>
     </div>
   );
-}
+};
 
 export default NavBar;
