57ecffeea6d167502dfb5938145fa8acdd7f6abc
whobutjg <jgiardinajg@gmail.com>
2021-04-19 22:55:39 -0400

    saves about.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,15 +4,15 @@ import typingEffect from './TypingEffect';
 const About = () => {
   return ( 
     <>
-    <div id="about">
-      <p>Hey, there!</p>
-      <p>I'm Joseph Giardina, but everyone calls me 'Joey'.</p>
-      <p>I'm a {typingEffect}from New Jersey now residing in sunny South Florida.</p>
-      <p>For collaboration, consultation, or to connect, feel free to use the 'contact me' form or connect via LinkedIn.</p>
-    </div>
-    <div id="form">
-
-    </div>
+      <div id="about">
+        <p>Hey, there!</p>
+        <p>I'm Joseph Giardina, but everyone calls me 'Joey'.</p>
+        <p>I'm a {typingEffect}from New Jersey now residing in sunny South Florida.</p>
+        <p>For collaboration, consultation, or to connect, feel free to use the 'contact me' form or connect via LinkedIn.</p>
+      </div>
+      <div id="form">
+        <form action=""></form>
+      </div>
     </>
     )      
 }
